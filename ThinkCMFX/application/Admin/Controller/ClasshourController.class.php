e59620923111a5b8813620fffd4a14b0a2e3600c
liuguodong <331787444@qq.com>
2017-05-05 20:47:21 +0800

    no message

--- ThinkCMFX/application/Admin/Controller/ClasshourController.class.php
+++ ThinkCMFX/application/Admin/Controller/ClasshourController.class.php
@@ -10,11 +10,29 @@ class ClasshourController extends AdminbaseController
      */
     public function show ()
     {
-        echo 666;die;
         $this->display();
     }
 
+    /**
+     * 添加课时
+     */
     public function create ()
+    {
+        $this->display();
+    }
+
+    /**
+     * 修改课时
+     */
+    public function update ()
+    {
+        $this->display();
+    }
+
+    /**
+     * 删除课时
+     */
+    public function delete ()
     {
 
     }
