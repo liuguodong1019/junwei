ed21cf55d1b1873a49e05894a9eb741bb65f8d00
liuguodong <331787444@qq.com>
2017-05-05 20:46:52 +0800

    no message

---
+++ ThinkCMFX/application/Admin/Controller/ClasshourController.class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Admin\Controller;
+use Common\Controller\AdminbaseController;
+
+class ClasshourController extends AdminbaseController
+{
+    /**
+     * 课时列表
+     */
+    public function show ()
+    {
+        echo 666;die;
+        $this->display();
+    }
+
+    public function create ()
+    {
+
+    }
+}
