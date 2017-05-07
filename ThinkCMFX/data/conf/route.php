3877f6cb7631f6fde00c49dec7f2343c4daba959
liuguodong <331787444@qq.com>
2017-05-07 18:05:19 +0800

    no message

--- ThinkCMFX/data/conf/route.php
+++
@@ -1,7 +0,0 @@
-<?php	return array (
-  'get_class_list' => 'api/course/get_class_list',
-  'get_class' => 'api/course/get_class',
-  'open_class' => 'api/course/open_class',
-  'vip' => 'api/course/vip',
-  'past_live' => 'api/course/past_live',
-);
